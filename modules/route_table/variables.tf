75a4f1adc194eb5a7875d163c42d1642d63ef6be
Bazelden <jack.waller@hotmail.com>
2024-07-08 16:22:08 +0100

    fixed route table variable issue

--- modules/route_table/variables.tf
+++ modules/route_table/variables.tf
@@ -1,4 +1,8 @@
-subnet_count = {
-  public  = 2  # Adjust the number of public subnets as needed
-  private = 2  # Adjust the number of private subnets as needed hey jack!
+variable "subnet_count" {
+  description = "The number of public and private subnets to create"
+  type        = map(number)
+  default = {
+    public  = 2 # Adjust the number of public subnets as needed
+    private = 2 # Adjust the number of private subnets as needed hey jack!
+  }
 }
